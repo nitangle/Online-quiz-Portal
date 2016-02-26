2540659e6643e63b612d400bbf55fedc2f090777
Rupanshu <vermarupanshu733@gmail.com>
2016-02-26 19:25:06 +0530

    Registration Form  -stage 1 of registration is complited

--- Exam_portal/views.py
+++ Exam_portal/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import  render,get_object_or_404,redirect
+
+from .forms import RegistrationForm
+
+def register(request):
+    form  = RegistrationForm()
+
+    context = {
+        'title':'SI recruitment',
+        'form':form,
+    }
+    return render(request,'Exam_portal/registration.html',context)
