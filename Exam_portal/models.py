3a7aff703ca6998465daadc15bb4c0f4365db6c5
Nishtha <nishtha1896@gmail.com>
2016-02-26 19:05:18 +0530

    models completed stage 1

--- Exam_portal/models.py
+++ Exam_portal/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Students(models.Model):
+    name = models.CharField(max_length=20)                                  # Stage1 of registration
+    student_no = models.IntegerField()                                      # Stage2 of registration
+    branch = models.CharField(max_length=5)                                 # Stage2 of registration
+    contact = models.BigIntegerField()                                      # Stage1 of registration
+    skills = models.CharField(max_length=10)                                # Stage3 of registration
+    email = models.EmailField()                                             # Stage1 of registration
+    hosteler = models.BooleanField()                                        # Stage2 of registration
+    designer = models.CharField(label="Mention any software you worked on(photoshop etc)", max_length=10) # Stage3 of registration
+
+
+class Categories(models.Model):
+    category = models.CharField(max_length=10)
+
+
+class Questions(models.Model):
+    question_text = models.CharField(max_length=10)
+    negative = models.BooleanField()
+    marks = models.IntegerField()
+    type = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+
+class QuestionChoices(models.Model):
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=20)
+
+
+class CorrectChoices(models.Model):
+    question_id = models.ForeignKey(Questions, on_delete=models.CASCADE, db_column='question_id')
+    correct_choice = models.ForeignKey(QuestionChoices, on_delete=models.CASCADE)
+
+
+class StudentAnswers(models.Model):
+    answer = models.CharField(max_length=20)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    student = models.ForeignKey(Students, on_delete=models.CASCADE)
+
+
+class Test(models.Model):
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+
+'''
+class Instructions(models.Model):
+    instructions = models.CharField()
+
+'''
+
+
--- online_exam/settings.py
+++ online_exam/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'l@*e3$zi_8n+zo#e0xa!bhkihf2%vpm_s=q#r$7)9l2q%_c+a3'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,22 +68,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'online_exam.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-	'ENGINE': 'django.db.backends.postgresql',        
-	'NAME': os.environ['NAME'],
-	'USER': os.environ['USER'],
-	'PASSWORD':os.environ['PASSWORD'],
-	'HOST':os.environ['HOST'],
-	'PORT':os.environ['PORT'],
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['NAME'],
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': os.environ['HOST'],
+        'PORT': os.environ['PORT'],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
@@ -118,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
