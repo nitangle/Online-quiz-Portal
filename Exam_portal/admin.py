601d714e7aa8996ab0df76be2d4ecaa1ab53bafb
Rupanshu <vermarupanshu733@gmail.com>
2016-02-27 17:48:48 +0530

    Phase 1 for registration form is completed

--- Exam_portal/admin.py
+++ Exam_portal/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import *
+
+
+
 # Register your models here.
+
+admin.site.register(QuestionChoice)
+admin.site.register(Question)
+admin.site.register(Test)
+admin.site.register(Category)
+admin.site.register(CorrectChoice)
+admin.site.register(Student)
+admin.site.register(StudentAnswer)
--- Exam_portal/models.py
+++ Exam_portal/models.py
@@ -1,52 +1,57 @@
 from django.db import models
 
 
-class Students(models.Model):
-    name = models.CharField(max_length=20)                                  # Stage1 of registration
-    student_no = models.IntegerField()                                      # Stage2 of registration
-    branch = models.CharField(max_length=5)                                 # Stage2 of registration
-    contact = models.BigIntegerField()                                      # Stage1 of registration
-    skills = models.CharField(max_length=10)                                # Stage3 of registration
-    email = models.EmailField()                                             # Stage1 of registration
-    hosteler = models.BooleanField()                                        # Stage2 of registration
-    designer = models.CharField(label="Mention any software you worked on(photoshop etc)", max_length=10) # Stage3 of registration
+class Student(models.Model):
+    name = models.CharField(max_length=20)  # Stage1 of registration
+    student_no = models.IntegerField()  # Stage2 of registration
+    branch = models.CharField(max_length=5)  # Stage2 of registration
+    contact = models.BigIntegerField()  # Stage1 of registration
+    skills = models.CharField(max_length=225)  # Stage3 of registration
+    email = models.EmailField()  # Stage1 of registration
+    hosteler = models.BooleanField()  # Stage2 of registration
+    designer = models.CharField(
+        "Mention any software you worked on(photoshop etc)",
+        max_length=10)  # Stage3 of registration
 
 
-class Categories(models.Model):
+class Category(models.Model):
     category = models.CharField(max_length=10)
 
+    class Meta:
+        verbose_name_plural = "Categories"
 
-class Questions(models.Model):
+
+class Question(models.Model):
     question_text = models.CharField(max_length=10)
     negative = models.BooleanField()
     marks = models.IntegerField()
-    type = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    type = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
-class QuestionChoices(models.Model):
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+class QuestionChoice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice = models.CharField(max_length=20)
 
 
-class CorrectChoices(models.Model):
-    question_id = models.ForeignKey(Questions, on_delete=models.CASCADE, db_column='question_id')
-    correct_choice = models.ForeignKey(QuestionChoices, on_delete=models.CASCADE)
+class CorrectChoice(models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE,
+                                    db_column='question_id')
+    correct_choice = models.ForeignKey(QuestionChoice, on_delete=models.CASCADE)
 
 
-class StudentAnswers(models.Model):
+class StudentAnswer(models.Model):
     answer = models.CharField(max_length=20)
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
-    student = models.ForeignKey(Students, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 
 class Test(models.Model):
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
+
 
 '''
 class Instructions(models.Model):
     instructions = models.CharField()
 
 '''
-
-
